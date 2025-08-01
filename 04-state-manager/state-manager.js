02550663c2654e435d850e99092adde6fd7522e3
Magesh Kuppan <tkmagesh77@gmail.com>
2025-08-01 14:46:51 +0530

    updates

--- 04-state-manager/state-manager.js
+++ 04-state-manager/state-manager.js
@@ -35,5 +35,16 @@ const StateManager = (() => {
     };
   }
 
-  return {createStore}
+  function bindActionCreators(actionCreators, dispatch) {
+    const actionDispatchers = {};
+    for (let key in actionCreators) {
+      actionDispatchers[key] = function (...args) {
+        const action = actionCreators[key](...args);
+        dispatch(action);
+      };
+    }
+    return actionDispatchers;
+  }
+
+  return { createStore, bindActionCreators };
 })()
